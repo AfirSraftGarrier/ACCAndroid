c29188795f97dc0eaf7e008c1ff76ff30026a975
AfirSraftGarrier <AfirSraftGarrier@gmail.com>
2014-09-13 18:15:44 +0800

    Change ACCBaseActivity Name.

--- src/com/acc/android/frame/activity/base/ACCBaseActivity.java
+++ src/com/acc/android/frame/activity/base/ACCBaseActivity.java
@@ -3,7 +3,7 @@ package com.acc.android.frame.activity.base;
 import android.app.Activity;
 import android.view.View;
 
-public class ACCBaseActvity extends Activity {
+public class ACCBaseActivity extends Activity {
 	public void back(View view) {
 		this.finish();
 	}
